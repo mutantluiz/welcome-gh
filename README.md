7eb3f365d820ecb26ddc696d6ace4ad211b5acff
Luiz Filho <luiz.filho@mutantbr.com>
2019-08-08 22:25:39 -0300

    add posts and authors

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # welcome-gh
+
+Access GraphiQL on https://graphql-server-express.glitch.me/graphql
+
+And try the following query
+
+{
+  post(id: 1) {
+    title
+    description
+    author {
+      name
+      age
+    }
+  },
+  posts {
+    title
+    description
+    author {
+      name
+      age
+    }
+  }
+}
--- schema.js
+++ schema.js
@@ -1,18 +1,87 @@
 const graphql = require('graphql')
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt
+} = graphql
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'RootQueryType',
-        fields: {
-            hello: {
-                type: GraphQLString,
-                resolve() {
-                    return 'GRAPHQL'
-                }
+
+
+const posts = [
+    {
+        title: 'Javascript post',
+        description: 'Ecmascript 2015',
+        author: 'Luiz'
+    },
+    {
+        title: 'Python',
+        description: 'artificial intelligence',
+        author: 'John'
+    }
+]
+
+const authors = {
+    'Luiz': {
+        name: 'Luiz Filho',
+        age: 30
+    },
+    'John': {
+        name: 'John Doe',
+        age: 31
+    }
+}
+
+const authorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+})
+
+const postType = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+        title: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        },
+        author: {
+            type: authorType,
+            resolve: (source, params) => {
+                return authors[source.author]
             }
         }
-    })
+    }
+})
+
+const queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        post: {
+            type: postType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (source, { id }) => {
+                return posts[id]
+            }
+        },
+        posts: {
+            type: new GraphQLList(postType),
+            resolve: () => {
+                return posts
+            }
+        }
+    }
+})
+
+const schema = new GraphQLSchema({
+    query: queryType
 })
 
 module.exports = schema
